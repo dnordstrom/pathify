b995333054f351682142f2d2bb6b1d2897a353a1
Daniel Nordstrom <d@nintera.com>
2012-01-22 00:56:08 +0100

    .

--- lib/pathify/bundle.rb
+++ lib/pathify/bundle.rb
@@ -2,7 +2,7 @@ module Pathify
   class Bundle < Finder
     def initialize(cmd)
       @path = `bundle show #{cmd}`
-      raise RuntimeError unless File.exists?(@path)
+      #raise RuntimeError unless File.exists?(@path)
     end
   end
 end
