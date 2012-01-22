14bfbba1235d62c82a76796830c6bc1e6bf6811c
Daniel Nordstrom <d@nintera.com>
2012-01-22 01:39:26 +0100

    Correct Gem directory path.

--- lib/pathify/gem.rb
+++ lib/pathify/gem.rb
@@ -2,7 +2,7 @@ module Pathify
   class Gem < Finder
     def initialize(cmd)
       output = `gem which #{cmd}`
-      @path = File.expand_path(File.join(File.dirname(output) + '..')) if output =~ /^\//
+      @path = File.expand_path(File.join(File.dirname(output), '..')) if output =~ /^\//
     end
   end
 end
