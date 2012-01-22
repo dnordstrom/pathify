3b1d37779ac1e1f91d84283587395e890171dc39
Daniel Nordstrom <d@nintera.com>
2012-01-22 01:10:45 +0100

    Adding exception.

--- lib/pathify/bundle.rb
+++ lib/pathify/bundle.rb
@@ -2,7 +2,7 @@ module Pathify
   class Bundle < Finder
     def initialize(cmd)
       @path = `bundle show #{cmd}`
-      puts "\n\n#{@path}\nExists? #{File.exists(@path)}\n\n"
+      print "\n\n#{@path}\nExists? #{File.exists(@path)}\n\n"
       #raise RuntimeError unless File.exists?(@path)
     end
   end
