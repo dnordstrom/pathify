a32a089b93697d55968c41cfa1fb73944473554f
Daniel Nordstrom <d@nintera.com>
2012-01-22 01:20:00 +0100

    .

--- lib/pathify/bundle.rb
+++ lib/pathify/bundle.rb
@@ -2,6 +2,7 @@ module Pathify
   class Bundle < Finder
     def initialize(cmd)
       output = `bundle show #{cmd}`
+      print "\n\n#{output}\n\n"
       @path = output if FileTest.exists? output
     end
   end
